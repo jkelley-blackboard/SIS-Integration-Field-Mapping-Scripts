2278ed4134edd2cc6da9c7b89bb5dac299bd70f5
JeffK@Anthology <jeff.kelley@anthology.com>
2026-01-16 10:26:36 -0500

    Update logging messages for primary institution roles

--- javascripts/LIS_primaryInstitutionRoles.js
+++ javascripts/LIS_primaryInstitutionRoles.js
@@ -1,28 +1,32 @@
 (function() { 
+// Blackboard SIS Mapping Script
+// Correctly maps primary institution roles
+ var sField = 'Primary Institution Role: ';  //login prefix
+ 
  var primaryRole = data.getPerson().getRoles().get(0).getPrimaryInstitutionRole(); 
  if ( primaryRole != null ) { 
    var instRole = primaryRole.value; 
    helper.logInfo("Primary Role found in XML = " +  instRole );
      
    if ( instRole == 'Student' ) {
-             helper.logInfo("Mapping Primary Institution Role to STUDENT");
+             helper.logInfo(sField + "Mapping to STUDENT");
              return 'STUDENT'; 
       }
    else if ( instRole == 'Instructor' ) {
-             helper.logInfo("Mapping Primary Institution Role to FACULTY");
+             helper.logInfo(sField + "Mapping to FACULTY");
              return 'FACULTY';
       }
    else if ( instRole == 'Staff' ) {
-             helper.logInfo("Mapping Primary Institution Role to STAFF");
+             helper.logInfo(sField + "Mapping to STAFF");
              return 'STAFF';
       } 
    else {
-             helper.logInfo("No map for Primary Institution Role");
+             helper.logInfo(sField + "No map value.");
              return instRole; 
    }
  }  
  else { 
-   helper.logWarning("Primary Role not found in XML.  Skip Record");
+   helper.logWarning(sField + "Role not found.  Skip Record");
    helper.skipRecord();
  }
 }()); 
